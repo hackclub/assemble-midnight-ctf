82543e9f64901e3d3092c647e2a5dd835dc54f70
Benjamin Ashbaugh <hi@benjaminashbaugh.me>
2022-08-05 02:51:23 -0700

    hack.af redirect

--- midnight/next.config.js
+++ midnight/next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  redirects: [
+    {
+      source: "/auth/start",
+      destination: "/",
+      permanent: false,
+    },
+  ],
   webpack: (config) => {
     config.module.rules.push({
       test: /\.md$/,
