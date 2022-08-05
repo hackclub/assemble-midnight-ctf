831767c982a9796ad143b68a59fa16a02dbd23f6
Benjamin Ashbaugh <hi@benjaminashbaugh.me>
2022-08-05 11:18:43 -0700

    redis env

--- server/src/redis.ts
+++ server/src/redis.ts
@@ -1,6 +1,8 @@
 import { createClient } from "redis";
 
-const client = createClient();
+const client = createClient({
+  url: process.env.REDIS_URL,
+});
 
 client.on("error", (err) => {
   console.log("Error " + err);
